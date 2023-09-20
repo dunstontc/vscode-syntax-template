e1f90d4c1f4ae153e4e5196e1986982c391f9885
Clay Dunston <claydunston@gmail.com>
2023-09-20 13:26:28 -0500

    [WIP] remove gulp

--- src/build.js
+++ src/build.js
@@ -1,6 +1,9 @@
-const fs = require("fs")
+const fs = require("fs/promises")
 const path = require("path")
+
 const coolkit = require("coolkit-js")
+const fixJson = require("fixjson")
+const _ = require("lodash")
 
 const PROJECT_ROOT = process.cwd()
 
@@ -8,7 +11,20 @@ const DEFS_FOLDER = path.join(PROJECT_ROOT, "src", "definitions")
 
 const buildAsync = async () => {
     const files = coolkit.readFolder(DEFS_FOLDER, true)
-    console.log(files)
+
+    const definitions = []
+
+    for (const filePath of files) {
+        const buffer = await fs.readFile(filePath)
+        const string = buffer.toString()
+        const cleanString = fixJson.fixString(string)
+        const json = JSON.parse(cleanString)
+        definitions.push(json)
+    }
+
+    const merged = _.merge({}, ...definitions)
+
+    console.log(JSON.stringify(merged, null, 4))
 }
 
 module.exports = {
